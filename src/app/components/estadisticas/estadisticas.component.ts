f616e8c2cb519286634d43bc2a59e5435329866e
Angel Oyola <angeloyola00@gmail.com>
2025-05-30 23:21:59 -0500

    Commit

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -1,3 +1,20 @@
+/*
+4.1.
+1. En que archivo se define la interfaz del juego?
+en el src/app/interfaces/juego.interface.ts
+2.Que archivo maneja el estado global de los filtros?
+en el src/app/components/lista-juegos/lista-juegos.component.ts
+3. Donde se configura el HttpClient para la aplicacion?
+En el main
+*/ 
+/*
+4.2.
+1. Por que este proyecto no tiene app.module.ts?
+Porque la app usa standalone
+2. Que ventaja tiene usar BehaviorSubject en el servicio de juegos?
+Se mantiene el estado actual de filtros y siempre da el valor actual.
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,7 +26,3 @@ import { Component } from '@angular/core';
 export class EstadisticasComponent {
 
 }
-/*
-1. En que archivo se define la interfaz de juego
-
-*/
